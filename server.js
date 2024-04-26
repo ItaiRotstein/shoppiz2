ebd00b18bacb6b1fdfa96cd36e9361f865104261
Itai Rotstein <itairotstein@gmail.com>
2024-04-27 00:51:30 +0300

    init2

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ connectDB();
 const app = express();
 // app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/products', require('./api/product/productRoutes'));
 app.use('/api/users', require('./api/user/userRoutes'));
